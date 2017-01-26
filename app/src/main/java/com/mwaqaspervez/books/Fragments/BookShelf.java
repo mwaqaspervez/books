9cf56a650719181830a03290a1fa24ab02374e56
mwaqaspervez <m.waqaspervez@gmail.com>
2017-01-26 14:21:14 +0500

    added some drawer items added some pdf extensions

--- app/src/main/java/com/mwaqaspervez/books/Fragments/BookShelf.java
+++ app/src/main/java/com/mwaqaspervez/books/Fragments/BookShelf.java
@@ -1,5 +1,6 @@
 package com.mwaqaspervez.books.Fragments;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,14 +12,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.barteksc.pdfviewer.PDFView;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import com.mwaqaspervez.books.R;
 import com.mwaqaspervez.books.Utils.Util;
 import com.tom_roush.pdfbox.pdmodel.PDDocument;
-import com.tom_roush.pdfbox.text.PDFTextStripper;
+import com.tom_roush.pdfbox.rendering.PDFRenderer;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.URI;
 
 public class BookShelf extends Fragment {
     private TextView textView;
@@ -31,32 +38,60 @@ public class BookShelf extends Fragment {
 
         textView = new TextView(getContext());
         textView.setMovementMethod(new ScrollingMovementMethod());
+        //new myTask().execute();
+
+        PDFView pdfView = (PDFView) rootView.findViewById(R.id.pdf_view);
+
+
+       /* pdfView.fromFile(imagelist[0])
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false)
+                .password(null)
+                .scrollHandle(null)
+                .load();*/
         new myTask().execute();
+
         return textView;
     }
 
-    public String stripText(File file) {
+    public Bitmap stripText(File file) {
         String parsedText = null;
         PDDocument document = null;
         try {
             document = PDDocument.load(file);
+
+            PDFRenderer renderer = new PDFRenderer(document);
+            // Render the image to an RGB Bitmap
+            Bitmap pageImage = renderer.renderImage(0, 1, Bitmap.Config.RGB_565);
+            File root = android.os.Environment.getExternalStorageDirectory();
+            String path = root.getAbsolutePath() + "/Download/render.jpg";
+            File renderFile = new File(path);
+            FileOutputStream fileOut = new FileOutputStream(renderFile);
+            pageImage.compress(Bitmap.CompressFormat.JPEG, 100, fileOut);
+            fileOut.close();
+
+
+            return pageImage;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try {
+      /*  try {
             PDFTextStripper pdfStripper = new PDFTextStripper();
 
             parsedText = "Parsed text: " + pdfStripper.getText(document);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
+        }*/ finally {
             try {
                 if (document != null) document.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return "ERROR";
+        return null;
     }
 
     public class myTask extends AsyncTask<Void, Void, String> {
@@ -72,13 +107,26 @@ public class BookShelf extends Fragment {
 
         @Override
         protected String doInBackground(Void... voids) {
+            String text = "";
             File images = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
             File[] imagelist = images.listFiles(new FilenameFilter() {
                 public boolean accept(File dir, String name) {
                     return ((name.endsWith(".pdf")));
                 }
             });
-            return stripText(imagelist[1]);
+            PdfReader reader = null;
+            try {
+                reader = new PdfReader(URI.create("http://www.nsaahome.org/textfile/journ/multipdf.pdf").toURL());
+                PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+
+                for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+                    text += (PdfTextExtractor.getTextFromPage(reader, i));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return text;
         }
 
         @Override
@@ -88,7 +136,6 @@ public class BookShelf extends Fragment {
             Util.Log("Time Ended : " + endTime);
             Util.Log("Time Taken : " + (endTime - startTime) / 1000);
             textView.setText(s);
-
         }
     }
 }
---
+++ app/src/main/java/com/mwaqaspervez/books/Models/imagegetter.java
@@ -0,0 +1,28 @@
+package com.mwaqaspervez.books.Models;
+
+
+import com.itextpdf.text.pdf.parser.ImageRenderInfo;
+import com.itextpdf.text.pdf.parser.RenderListener;
+import com.itextpdf.text.pdf.parser.TextRenderInfo;
+
+public class imagegetter implements RenderListener {
+    @Override
+    public void beginTextBlock() {
+
+    }
+
+    @Override
+    public void renderText(TextRenderInfo renderInfo) {
+
+    }
+
+    @Override
+    public void endTextBlock() {
+
+    }
+
+    @Override
+    public void renderImage(ImageRenderInfo renderInfo) {
+
+    }
+}
